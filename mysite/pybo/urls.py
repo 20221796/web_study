e024b12e85255bf1c07397cfafdf57d47beebdd6
20221796 <tjwlsqo44@gmail.com>
2024-01-12 22:28:21 +0900

    feat(backend) : 질문 페이지 생성

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question.id>/', views.detail),
 ]
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.") 
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list':question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
